dcce72bca6977f0bb021f146b6d2079d1b4719b2
GhostDeini <45671983+GhostDeini@users.noreply.github.com>
2022-10-05 11:48:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+version = '0.0.0'
+packages = ['perex']
+
+setup(name='perex',
+      version=version,
+      description='Python Environment for Relating Electrochemistry and XAS data',
+      author='Lucía Pérez Ramírez',
+      author_email='lucia.perez@synchrotron-soleil.fr',
+      #long_description=long_description,
+      license='GPL >= v.3',
+      platforms=['Linux', 'Windows'],
+      url="https://github.com/GhostDeini/perex",
+      packages=packages,
+      python_requires='>=3.9',
+      #package_data=package_data,
+      #scripts=scripts
+      )
