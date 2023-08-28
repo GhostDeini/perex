4746bb72b49d89262699bb774178c0abce53177a
GhostDeini <45671983+GhostDeini@users.noreply.github.com>
2023-08-28 17:26:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 #with open("README.md", "r") as fh:
 #    long_description = fh.read()
 
-version = '1.0.0'
-packages = ['perex']
+version = '5.1.0'
+packages = ['perex','perex.aux']
 
 setup(name='perex',
       version=version,
+      packages=packages,
       description='Python Environment for Relating Electrochemistry and XAS data',
       author='Lucía Pérez Ramírez',
       author_email='lucia.perez@synchrotron-soleil.fr',
@@ -15,7 +16,6 @@ setup(name='perex',
       license='GPL >= v.3',
       platforms=['Linux', 'Windows'],
       url="https://github.com/GhostDeini/perex",
-      packages=packages,
       python_requires='>=3.9',
       #package_data=package_data,
       #scripts=scripts
